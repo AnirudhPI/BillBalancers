797200e993cbff1f0038b0a1ded3d644594a0883
AnirudhPI <anirudhinamdar@gmail.com>
2024-01-25 22:19:43 -0700

    Override methods

--- authenticator/authenticator-app/src/main/java/com/billbalancers/authenticator/controller/RestController.java
+++ authenticator/authenticator-app/src/main/java/com/billbalancers/authenticator/controller/RestController.java
@@ -1,7 +1,13 @@
 package com.billbalancers.authenticator.controller;
 
 import com.billbalancers.authenticatorapi.api.StudentApi;
+import com.billbalancers.authenticatorapi.model.Student;
+import org.springframework.http.ResponseEntity;
 
 public class RestController implements StudentApi {
 
+    @Override
+    public ResponseEntity<Student> getStudentDetail() {
+        return null;
+    }
 }
