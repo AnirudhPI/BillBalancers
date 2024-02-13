e4fd927c8f3c430d4bf04e9a3a4349095e7308e7
AnirudhPI <anirudhinamdar@gmail.com>
2024-02-13 14:37:38 -0700

    Structured go service

--- expense-service/internal/expense-service/handler.go
+++ expense-service/internal/expense-service/handler.go
@@ -2,6 +2,7 @@ package expenseservice
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/AnirudhPI/BillBalancers/proto"
 )
@@ -13,4 +14,10 @@ type Microservice struct {
 func (ms *Microservice) SayHello(ctx context.Context, req *proto.HelloRequest) (*proto.HelloResponse, error) {
 	name := req.GetName()
 	return &proto.HelloResponse{Greeting: name}, nil
+}
+
+func (ms *Microservice) CreateGroup(ctx context.Context, req *proto.GroupName) (*proto.Group, error) {
+	name := req.GetGroupName()
+	fmt.Println(name)
+	return &proto.Group{groupName: name}, nil
 }
